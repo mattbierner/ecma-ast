b8afdd2024bd462f5fe2c04e5bfb4e4ce6f7df40
Matt Bierner <mattbierner@gmail.com>
2014-01-22 15:38:27 -0500

    Added index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'clause': require('./lib/clause'),
+    'declaration': require('./lib/declaration'),
+    'expression': require('./lib/expression'),
+    'node': require('./lib/node'),
+    'position': require('./lib/position'),
+    'program': require('./lib/program'),
+    'serialization': require('./lib/serialization'),
+    'statement': require('./lib/statement'),
+    'token': require('./lib/token'),
+    'value': require('./lib/value')
+};
